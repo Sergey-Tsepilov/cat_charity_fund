60902053d10432528d51388e91280bbf6674a0bc
Sergey Tsepilov <st08kz@yandex.kz>
2025-01-12 21:27:04 +0500

    donation

---
+++ app/crud/donation.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.crud.base import CRUDBase
+from app.models import Donation, User
+
+
+class CRUDDonation(CRUDBase):
+    """Класс для операций CRUD с пожертвованиями."""
+
+    async def get_by_user(
+        self,
+        user: User,
+        session: AsyncSession,
+    ) -> List[Donation]:
+        """Получение всех пожертвований пользователя по его ID."""
+        donations = await session.execute(
+            select(Donation).where(
+                Donation.user_id == user.id,
+            )
+        )
+        return donations.scalars().all()
+
+
+crud_donation = CRUDDonation(Donation)
