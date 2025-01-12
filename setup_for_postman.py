96b7fc56060be7c9eee5b895aa9b4ae35e7ed88a
Sergey Tsepilov <st08kz@yandex.kz>
2025-01-12 21:33:11 +0500

    setup_for_postman

--- setup_for_postman.py
+++ setup_for_postman.py
@@ -2,25 +2,25 @@ import asyncio
 
 try:
     from app.core.init_db import create_user
-except (ImportError, NameError):
+except (ImportError, NameError) as e:
     raise ImportError(
         'Не удалось импортировать функцию `create_user` из модуля '
         '`app.core.init_db`. Создайте суперпользователя самостоятельно.'
-    )
+    ) from e
 
 
 class UserCreationError(Exception):
-    pass
+    """Ошибка при создании пользователя."""
 
 
 if __name__ == '__main__':
     try:
         asyncio.run(create_user('root@admin.ru', 'root', is_superuser=True))
-    except Exception:
+    except Exception as e:
         raise UserCreationError(
             'Не удалось создать суперпользователя. Создайте '
             'суперпользователя самостоятельно, используя следующие учетные '
             'данные:\n'
             'email: root@admin.ru\n'
             'password: root'
-        )
+        ) from e
